12ea436fedd753df8c9a177f2c9fedceb9e16c4f
ludwig <lute_p@hotmail.com>
2017-07-22 21:43:34 +0800

    refer to README

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// this is the main class. this class is executed first when the program is run.
+		
+		new ReverseString();
+	}
+
+}
--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class ReverseString {
+	private static String userInput;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public ReverseString() {
+		System.out.println("In this program, I'll take in a word and give you a reverse of it.");
+		System.out.printf("Please give me a word: ");
+		
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		
+		userInput = sc.nextLine();
+		
+		System.out.println("Your new word is: " + reverse(userInput));
+	}
+	
+	public static String reverse(String input) {
+		char[] in = input.toCharArray(); // splits the inputed string into a char array
+		int begin = 0; // begin and end are counters 
+		int end = in.length - 1;
+		char temp; // temporary variable
+		while(end > begin) { // this swaps the characters in place of their neighbors in the array
+			temp = in[begin];
+			in[begin] = in[end];
+			in[end] = temp;
+			end--;
+			begin++;
+		}
+		return new String(in); // returns the newly formatted String
 	}
 
 }
